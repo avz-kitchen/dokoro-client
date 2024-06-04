10b651c1079172e636be06c7538c4eaabe15e2cc
AValenzue <zuellagraphy@gmail.com>
2024-06-04 17:23:19 +0200

    style sign up page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,8 +2,21 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import authService from "../services/auth.service";
+import {
+  Box,
+  Flex,
+  Stack,
+  Heading,
+  Text,
+  Container,
+  Input,
+  Button,
+  SimpleGrid,
+  // useBreakpointValue,
+  // Icon,
+} from "@chakra-ui/react";
 
-function SignupPage(props) {
+function Signup(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
@@ -19,45 +32,149 @@ function SignupPage(props) {
     e.preventDefault();
     const requestBody = { email, password, name };
 
-    authService
-      .signup(requestBody)
-      .then((response) => {
-        navigate("/login");
-      })
-      .catch((error) => {
+    async function signup() {
+      try {
+        await authService.signup(requestBody).then((response) => {
+          navigate("/login");
+        });
+      } catch (error) {
         const errorDescription = error.response.data.message;
         setErrorMessage(errorDescription);
-      });
+      }
+    }
+    signup();
   };
 
   return (
-    <div className="SignupPage">
-      <h1>Sign Up</h1>
+    <Box position={"relative"}>
+      <Container
+        as={SimpleGrid}
+        maxW={"7xl"}
+        columns={{ base: 1, md: 2 }}
+        spacing={{ base: 10, lg: 32 }}
+        py={{ base: 10, sm: 20, lg: 32 }}
+      >
+        <Stack spacing={{ base: 10, md: 20 }}>
+          <Heading
+            lineHeight={1.1}
+            fontSize={{ base: "3xl", sm: "4xl", md: "5xl", lg: "6xl" }}
+          >
+            Plant , grow
+            <Text
+              as={"span"}
+              bgGradient="linear(to-r, green.400,green.400)"
+              bgClip="text"
+            >
+              &
+            </Text>
+            process your plants
+          </Heading>
+        </Stack>
+        <Stack
+          bg={"gray.50"}
+          rounded={"xl"}
+          p={{ base: 4, sm: 6, md: 8 }}
+          spacing={{ base: 8 }}
+          maxW={{ lg: "lg" }}
+        >
+          <Stack spacing={4}>
+            <Heading
+              color={"gray.800"}
+              lineHeight={1.1}
+              fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
+            >
+              Plant your own garden
+              <Text
+                as={"span"}
+                bgGradient="linear(to-r, green.400,green.400)"
+                bgClip="text"
+              >
+                !
+              </Text>
+            </Heading>
+            <Text color={"gray.500"} fontSize={{ base: "sm", sm: "md" }}>
+              Create an account and start planting your garden. Find plants and
+              discover their power and effects.
+            </Text>
+          </Stack>
+          <Box as={"form"} onSubmit={handleSignupSubmit}>
+            <Stack spacing={4}>
+              <Input
+                type="text"
+                name="name"
+                value={name}
+                onChange={handleName}
+                placeholder="username"
+                bg={"gray.100"}
+                border={0}
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+              />
 
-      <form onSubmit={handleSignupSubmit}>
-        <label>Email:</label>
-        <input type="email" name="email" value={email} onChange={handleEmail} />
+              <Input
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleEmail}
+                placeholder="yourmail@mail.com"
+                bg={"gray.100"}
+                border={0}
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+              />
 
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={handlePassword}
-        />
+              <Input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handlePassword}
+                placeholder="Password"
+                bg={"gray.100"}
+                border={0}
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+              />
 
-        <label>Name:</label>
-        <input type="text" name="name" value={name} onChange={handleName} />
+              <Button
+                type="submit"
+                fontFamily={"heading"}
+                mt={8}
+                w={"full"}
+                bgGradient="linear(to-r, blue.400,green.400)"
+                color={"white"}
+                _hover={{
+                  bgGradient: "linear(to-r, green.400,green.400)",
+                  boxShadow: "xl",
+                }}
+              >
+                Sign Up
+              </Button>
 
-        <button type="submit">Sign Up</button>
-      </form>
+              {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
-
-      <p>Already have account?</p>
-      <Link to={"/login"}> Login</Link>
-    </div>
+              <Text color={"gray.500"} fontSize={{ base: "xs", sm: "xs" }}>
+                Already have account?
+              </Text>
+              <Link
+                to={"/login"}
+                color={"green.500"}
+                fontSize={{ base: "xs", sm: "xs" }}
+              >
+                {" "}
+                Login
+              </Link>
+            </Stack>
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
   );
 }
 
-export default SignupPage;
+export default Signup;
