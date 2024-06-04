9afa8465cfef9964b57d866f22a5d36cfd92500f
AValenzue <zuellagraphy@gmail.com>
2024-06-04 17:31:03 +0200

    fix error

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
   build: {
     rollupOptions: {
-      external: ['react-router-dom']
+      external: ["react-router-dom"]
     }
   }
 })
