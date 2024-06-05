3558131479e8cd7f2c500ed1280f13818668de6c
AValenzue <zuellagraphy@gmail.com>
2024-06-06 00:42:32 +0200

    render gardenlist

--- src/pages/Gardens.jsx
+++ src/pages/Gardens.jsx
@@ -2,13 +2,28 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { API_URL } from "../utils/constants";
+import {
+  Box,
+  Container,
+  Flex,
+  SimpleGrid,
+  Heading,
+  Card,
+  Text,
+  Button,
+  Center,
+  HStack,
+  Badge,
+  Square,
+  VStack,
+} from "@chakra-ui/react";
 
 function Gardens() {
   const [gardens, setGardens] = useState(null);
 
   const getGardens = () => {
     axios
-      .get(`${API_URL}/gardens`)
+      .get(`${API_URL}/api/gardens`)
       .then((response) => {
         const gardensFromApi = response.data.reverse();
         setGardens(gardensFromApi);
@@ -25,19 +40,68 @@ function Gardens() {
   }
 
   return (
-    <div>
-      <h1>Number of gardens: {gardens?.length}</h1>
+    <Box position={"relative"} spacing={{ base: 10, lg: 32 }}>
+      <HStack>
+        <Flex>
+          <VStack>
+            <Heading>My Gardens</Heading>
+            <Text>Number of gardens: {gardens?.length}</Text>
+          </VStack>
+          <Button>Build garden</Button>
+        </Flex>
+      </HStack>
 
-      {gardens?.map((garden) => {
-        return (
-          <Link to={`/gardens/${garden.id}`} key={garden.id}>
-            <div className="card">
-              <h3>{garden.title}</h3>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
+      <Container
+        as={SimpleGrid}
+        maxW={"7xl"}
+        columns={{ base: 1, md: 2 }}
+        spacing={{ base: 10, lg: 32 }}
+      >
+        {gardens?.map((garden, id) => {
+          return (
+            <Card
+              key={id}
+              spacing={{ base: 1, lg: 32 }}
+              maxW="420px"
+              bg="white"
+              p="6"
+            >
+              <Link to={`/gardens/:${garden._id}`} key={id}>
+                <Square
+                  bg="brand.700"
+                  borderRadius="base"
+                  minW="240px"
+                  minH="160px"
+                >
+                  <Heading as="h3" size="md" color="brand.900">
+                    {garden.title}
+                  </Heading>
+                </Square>
+                <br />
+
+                <HStack>
+                  <Text as="p" fontSize="xs" color="grey">
+                    Plants:
+                  </Text>
+                  <Badge as="span" color="green.800" fontSize="md">
+                    {garden.plant?.length ?? 0}
+                  </Badge>
+                  <Text as="p" fontSize="xs" color="grey">
+                    Location:
+                  </Text>
+                  <Badge as="span" color="green.800" fontSize="md">
+                    {garden.location}
+                  </Badge>
+                </HStack>
+                <Center my="6">
+                  <Button Linkcolor="green.800">Go to Garden</Button>
+                </Center>
+              </Link>
+            </Card>
+          );
+        })}
+      </Container>
+    </Box>
   );
 }
 
