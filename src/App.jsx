1bcf12350e8f1f7d6ee7c9542634bf81a2e88038
AValenzue <zuellagraphy@gmail.com>
2024-06-06 19:30:40 +0200

    provide title from pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,28 @@ import Login from "/src/pages/Login";
 import Signup from "./pages/Signup.jsx";
 import Gardens from "./pages/Gardens.jsx";
 import PlantGarden from "./pages/PlantGarden.jsx";
-import PlantDirectory from "./pages/PlantDirectory.jsx";
+import PlantDirectory from "./pages/PlantDirectory";
+import Plant from "./pages/Plant.jsx";
 import NewPlant from "./components/PlantForm.jsx";
 import AppLayout from "./Layout/appLayout.jsx";
-
+import { TitleProvider } from "./context/title.context.jsx";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="login" element={<Login />} />
-        <Route path="gardens" element={<Gardens />} />
-        <Route path="gardens/:gardenId" element={<PlantGarden />} />
-        <Route path="garden-new" element={<PlantGarden />} />
-        <Route path="plants" element={<PlantDirectory />} />
-        <Route path="plant-new" element={<NewPlant />} />
-      </Route>
-    </Routes>
+    <TitleProvider>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="gardens" element={<Gardens />} />
+          <Route path="gardens/:gardenId" element={<PlantGarden />} />
+          <Route path="garden-new" element={<PlantGarden />} />
+          <Route path="plants" element={<PlantDirectory />} />
+          <Route path="plants/:plantId" element={<Plant />} />
+          <Route path="plant-new" element={<NewPlant />} />
+        </Route>
+      </Routes>
+    </TitleProvider>
   );
 }
 
