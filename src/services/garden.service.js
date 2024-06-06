946f5619586ba619a6f3dd88e3908f7e4be3fa6c
AValenzue <zuellagraphy@gmail.com>
2024-06-06 15:06:04 +0200

    update garden api

--- src/services/garden.service.js
+++ src/services/garden.service.js
@@ -9,27 +9,27 @@ class GardenService {
 
     // Create a new garden
     createGarden = (gardenData) => {
-        return this.api.post("/gardens", gardenData);
+        return this.api.post("/api/gardens", gardenData);
     };
 
     // Get all gardens
     getGardens = () => {
-        return this.api.get("/gardens");
+        return this.api.get("/api/gardens");
     };
 
     // Get a specific garden by ID
     getGarden = (gardenId) => {
-        return this.api.get(`/gardens/${gardenId}`);
+        return this.api.get(`/api/gardens/${gardenId}`);
     };
 
     // Update a specific garden by ID
     updateGarden = (gardenId, updatedGardenData) => {
-        return this.api.put(`/gardens/${gardenId}`, updatedGardenData);
+        return this.api.put(`/api/gardens/${gardenId}`, updatedGardenData);
     };
 
     // Delete a specific garden by ID
     deleteGarden = (gardenId) => {
-        return this.api.delete(`/gardens/${gardenId}`);
+        return this.api.delete(`/api/gardens/${gardenId}`);
     };
 }
 
